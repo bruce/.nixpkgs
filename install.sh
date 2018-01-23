0e7e7eebaed190ccf9015a7b3c62aa1ec35cf700
Bruce Williams <brwcodes@gmail.com>
2018-01-22 18:16:03 -0800

    Try removal approach

--- install.sh
+++ install.sh
@@ -9,10 +9,16 @@ pushd $HOME > /dev/null
 
 set +e
 
-echo "Creating dotfiles links in user home"
+echo "Removing dotfiles link in user home"
+find .nix-profile/userHome/ -maxdepth 1 | \
+    sed "s/.nix-profile\/userHome\///g" | \
+    grep -v "^$" | xargs -I {} rm {}
+
+echo "Adding dotfiles link in user home"
 find .nix-profile/userHome/ -maxdepth 1 | \
     sed "s/.nix-profile\/userHome\///g" | \
     grep -v "^$" | xargs -I {} ln -sf .nix-profile/userHome/{} {}
+
 
 chown -R $USER .nix-profile
 
