bedd779a993a1760e03f56fc043cb478dca72b26
Bruce Williams <brwcodes@gmail.com>
2018-01-22 17:00:57 -0800

    Change error handling

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+
 set -e
 
 echo "Installing nix profile"
 nix-env -i all
 
 pushd $HOME > /dev/null
+
+set +e
 
 echo "Creating dotfiles links in user home"
 find .nix-profile/userHome/ -maxdepth 1 | sed "s/.nix-profile\/userHome\///g" | \
